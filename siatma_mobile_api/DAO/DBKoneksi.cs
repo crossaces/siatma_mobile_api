430317e046dea3d557ea62b2035ee1af10b4c710
WilliamLourensius <67148489+WilliamLourensius@users.noreply.github.com>
2021-10-25 12:06:11 +0700

    Update

--- siatma_mobile_api/DAO/DBKoneksi.cs
+++ siatma_mobile_api/DAO/DBKoneksi.cs
@@ -2,7 +2,7 @@
 {
     public class DBKoneksi
     {
-        public static string koneksi = "Server=192.168.15.156;Database=SIATMA_FTI;User Id=spkp_dev;Password=spkp123!";
+        public static string koneksi = "Server=192.168.15.156;Database=SIATMA_UAJY;User Id=spkp_dev;Password=spkp123!";
         
 
 
